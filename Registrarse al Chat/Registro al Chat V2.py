d08297fbb502d2914063bebb42bb8ca27fc2ecda
Rodrigo Checo <checorodrigo.cr@gmail.com>
2025-10-28 22:40:19 -0400

    Añadiendo cambios amigables en el codigo

--- Registrarse al Chat/Registro al Chat V2.py
+++ Registrarse al Chat/Registro al Chat V2.py
@@ -7,7 +7,7 @@ def obtenerNombreUsuario():
     nombre = input("Ingresa tu nombre o pulsa ENTER para generar uno automático: ").strip()
     if not nombre:
         nombre = generarNombreUsuario()
-    print(f"\n✅ Bienvenido al chat, Zorro {nombre}!")
+    print(f"\n✅ Bienvenido al chat, usuario {nombre} que disfrutes tu estancia en la misma!")
     print(f"\n✅ Creo que deberias irte del Chat, {nombre}")
     return nombre
 
